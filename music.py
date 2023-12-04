408416bfc76ad084f0ffcd152326a8860bfbc76c
dsblack0 <samhithad.1025@gmail.com>
2023-12-04 15:52:23 -0500

    music citations

--- music.py
+++ music.py
@@ -10,5 +10,7 @@ def loadSound(relativePath):
     # Load Sound file from local URL
     return Sound(url)
 
+# CITATION: https://www.youtube.com/watch?v=RQN1Pl1xUZM&ab_channel=ShadyInktail
 emeraldCity = loadSound('music/emeraldCity.mp3')
+# CITATION: https://www.youtube.com/watch?v=18qQrYMmrOg&list=PLx7hJyUUtt00zEzesvYlyyiJKJ3hqou-F&index=2&ab_channel=ShadyInktail
 skySummit = loadSound('music/skySummit.mp3')
